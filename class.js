4d7414f16be1501688bf80d2a15192351a7a606d
suvitcha <boom.suvitcha@gmail.com>
2025-07-18 22:47:48 +0700

    add(Class)Product

--- class.js
+++ class.js
@@ -10,6 +10,12 @@
 // - It should extend Error
 // - In constructor, call super(message) and set this.name = "ValidationError"
 // ✏️ YOUR CODE HERE
+class ValidationError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "ValidationError";
+  }
+}
 
 // ✅ STEP 2: Create a Product class
 // Requirements:
@@ -25,16 +31,57 @@
 // - getAge(): return days old since manufacturedDate
 // - static validateName(name): throw ValidationError if name is empty or shorter than 2 characters
 // ✏️ YOUR CODE HERE
+class Product {
+  #price = 0;
+  constructor(name, price, manufacturedDate) {
+    this.validateName=name;
+    this.price = price;
+    this.manufacturedDate=manufacturedDate;
+  }
+  get field() {
+    return this.#price;
+  }
+  set field(price) {
+    if (price < 0) throw new ValidationError("value<0");
+    this.#price = price;
+  }
+  getInfo() {
+    return this.validateName+"costs"+this.#price;
+  }
+  applyDiscount(percent){
+    const discount = Math.floor((this.price * percent) / 100);
+    return this.price=this.price-discount ;
+  }
+  getAge() {
+    return this.manufacturedDate
+  }
+  static validateName(name) {
+    if (!name) {
+      throw new ValidationError("Name must be at least 2 characters long and not empty.");
+    }
+    return true;
+  }
+}
 
 // ✅ STEP 3: Create subclasses LandProduct and SeaProduct
 // - both extend Product
 // - override getInfo() to prefix "[Land]" or "[Sea]" before the parent's getInfo()
 // ✏️ YOUR CODE HERE
-
+class LandProduct extends Product {
+ getInfo() {
+    //return "[Land]"; [Land] Apple costs $100
+    return "[Land] "+this.validateName+" costs "+this.price;
+  }
+}
+class SeaProduct extends Product {
+  getInfo() {
+    return "[Sea] "+this.validateName+" costs "+this.price;
+  }
+}
 // ✅ STEP 4: Test your code
 // After you finish implementing, uncomment the code below and run to see if it works.
 
-/*
+
 try {
   const apple = new LandProduct("Apple", 100, new Date(2025, 6, 1));
   console.log(apple.getInfo()); // [Land] Apple costs $100
@@ -48,7 +95,7 @@ try {
   console.log(tuna.getInfo()); // [Sea] Tuna costs $190
 
   // Uncomment below to test error
-  // Product.validateName(""); // should throw ValidationError
+   //Product.validateName(""); // should throw ValidationError
 } catch (err) {
   if (err instanceof ValidationError) {
     console.error("Validation failed:", err.message);
@@ -56,7 +103,7 @@ try {
     console.error("Error:", err.message);
   }
 }
-*/
+
 
 // ===============================
 // ✅ BONUS (Optional)
